f22a7f2a3e6c66f869f683a9b16354674d722f13
fcalve4 <freddycalve@gmail.com>
2025-04-01 14:15:15 -0400

    changed Dealer objects to inherit Player methods, added a method to get dealer upcard

--- app/game/dealer.py
+++ app/game/dealer.py
@@ -1,22 +1,9 @@
-class Dealer:
+from app.game.player import Player
+
+class Dealer(Player):
     def __init__(self):
         self.hand = []
 
 
-    def do_action(self, action):
-        """
-        Perform an action.
-        :param action: The action to be performed.
-        :return: None
-        """
-        print(f"{self.name} performs action: {action}")
-
-    def get_hand(self):
-        """
-        Get the dealer's hand.
-        :return: The dealer's hand (list of card objects).
-        """
-        return self.hand
-
-    def __repr__(self):
-        return f"{self.name}: {self.hand}"
+    def get_dealer_upcard(self):
+        return self.hand[0]
